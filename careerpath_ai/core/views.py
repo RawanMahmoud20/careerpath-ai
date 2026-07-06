b4b1cda82c6b31e2c503bb98a65d458829ed14fd
Mahmoud <mod2saman@gmail.com>
2026-07-06 16:52:29 +0300

    making the careers in the dashboard dynamic

--- careerpath_ai/core/views.py
+++ careerpath_ai/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from careers.models import Career
+
 
 def landing(request):
     """Public landing page with links to login and register."""
-    return render(request, "landing.html")
+    careers = Career.objects.all().order_by('title')
+    return render(request, "landing.html", {"careers": careers})
