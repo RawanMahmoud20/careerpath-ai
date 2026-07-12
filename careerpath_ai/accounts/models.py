fec4e37042b3a920f253a2e8638bf665281ec76d
AhmadAbd2020 <ahmadabdhaleem@gmail.com>
2026-07-12 21:28:16 +0300

    fix model

--- careerpath_ai/accounts/models.py
+++ careerpath_ai/accounts/models.py
@@ -5,6 +5,7 @@ class User(AbstractUser):
   
     full_name = models.CharField(max_length=150, blank=True, null=True, verbose_name="Full Name")
     email = models.EmailField(unique=True, verbose_name="Email Address")
+    is_email_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
